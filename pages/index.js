f7128f884e4e33046b4d28e52adc7036b3119285
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-16 16:09:19 -0700

    set up

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "@next/font/google"
 import styles from "@/styles/Home.module.css"
-
-const inter = Inter({ subsets: ["latin"] })
+import Header from "../components/Header"
 
 export default function Home() {
     return (
@@ -14,6 +11,8 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Header/>
+            {/* header / connect button / navbar */}
             Hello!
         </div>
     )
