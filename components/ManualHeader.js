f9c151a8df6ab12c9ddc87e229f9abe8beb13ab2
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-17 13:38:31 -0700

    use Header.js

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -3,12 +3,32 @@ import { useEffect } from "react"
 
 export default function ManualHeader() {
     // hook:
-    const { enableWeb3, account, isWeb3Enabled } = useMoralis()
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } = useMoralis()
 
     useEffect(() => {
-        console.log("Hi!")
-        console.log(isWeb3Enabled)
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+        enableWeb3()
     }, [isWeb3Enabled])
+    // no dependency array: run anytime something re-renders
+    // careful with this - don't want a circular render
+    // blank dependency array, run once on load
+    // dependencies in the array, run anytime something in there changes
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("Null account found")
+            }
+        })
+    }, [])
 
     return (
         <div>
@@ -20,7 +40,11 @@ export default function ManualHeader() {
                 <button
                     onClick={async () => {
                         await enableWeb3()
+                        if (typeof window !== "undefined") {
+                            window.localStorage.setItem("connected", "injected")
+                        }
                     }}
+                    disabled = {isWeb3EnableLoading}
                 >
                     Connect Wallet
                 </button>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import ManualHeader from "../components/ManualHeader"
+// import ManualHeader from "../components/ManualHeader"
+import Header from "../components/Header"
 
 export default function Home() {
     return (
@@ -11,7 +12,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <ManualHeader/>
+            <Header/>
             {/* header / connect button / navbar */}
             Hello!
         </div>
