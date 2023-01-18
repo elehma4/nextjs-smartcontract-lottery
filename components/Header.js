3ffb87668b8d924f3433be7e940f12ab234feb0d
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-18 16:11:48 -0700

    make slightly pretty

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
-import { ConnectButton } from "web3uikit";
+import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <div className="border-b-2">
-            Decentralized Lottery
-            <ConnectButton moralisAuth={false}/>
+        <div className="border-b-2 flex flex-row">
+            <h1 className="py-4 px-4 font-bold text-3xl">Decentralized Lottery</h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
         </div>
     )
 }
--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -7,7 +7,6 @@ import { ethers } from "ethers"
 // component library gives us code that's already formatted for us - for example includes button:
 import { useNotification } from "web3uikit"
 
-
 export default function LotteryEntrance() {
     const { chainId: chainIdHex, isWeb3Enabled, Moralis } = useMoralis()
     const chainId = parseInt(chainIdHex)
@@ -18,7 +17,11 @@ export default function LotteryEntrance() {
 
     const dispatch = useNotification()
 
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
@@ -84,23 +87,29 @@ export default function LotteryEntrance() {
     }
 
     return (
-        <div>
+        <div className="p-5">
             Hi from lottery entrance!
             {raffleAddress ? (
                 <div className="">
                     <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         onClick={async function () {
                             await enterRaffle({
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
                         }}
+                        disabled={isLoading || isFetching}
                     >
-                        Enter Raffle
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
                     </button>
-                    Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH Number Of
-                    Players: {numPlayers}
-                    Recent Winner: {recentWinner}
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+                    <div>Number Of Players: {numPlayers}</div>
+                    <div>Recent Winner: {recentWinner}</div>
                 </div>
             ) : (
                 <div>No Raffle Address Detected</div>
